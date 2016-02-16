01e918538a781534a4fd3ae87bae54ca04b55d7a
Caroline Wilgar <caroline.wilgar@roli.com>
2016-02-16 12:54:01 +0000

    just use index.twig for home page

--- web/app/themes/roli.com/index.php
+++ web/app/themes/roli.com/index.php
@@ -19,10 +19,4 @@ if ( ! class_exists( 'Timber' ) ) {
 }
 $context = Timber::get_context();
 $context['posts'] = Timber::get_posts();
-$context['foo'] = 'bar';
-$templates = array( 'index.twig' );
-if ( is_home() ) {
-	array_unshift( $templates, 'home.twig' );
-}
-
-Timber::render( $templates, $context );
+Timber::render( 'index.twig', $context );
